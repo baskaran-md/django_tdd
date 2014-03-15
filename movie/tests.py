03873ea56e1a708fe43ad7d4725d65828f37a6e5
Baskaran <baskaran.md@gmail.com>
2014-03-15 11:41:33 +0530

    Add more sample tests

--- movie/tests.py
+++ movie/tests.py
@@ -3,5 +3,10 @@ from django.test import TestCase
 class DemoTest(TestCase):
 
     def test_add(self):
-        self.assertEqual(10 + 10, 30)
+        self.assertEqual(10 + 10, 20)
 
+    def test_subtract(self):
+        self.assertEqual(10 - 10, 0)
+
+    def test_multiply(self):
+        self.assertEqual(10 * 10, 100)
