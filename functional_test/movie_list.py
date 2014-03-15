adbadcbbf837b6d45a5a501a8cf14d1e9f06d680
Baskaran <baskaran.md@gmail.com>
2014-03-15 11:24:09 +0530

    Refactor initial functional test

--- functional_test/movie_list.py
+++ functional_test/movie_list.py
@@ -1,8 +1,20 @@
 __author__ = 'baskar'
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("localhost:8000")
+class EndUserTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get("localhost:8000")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_add_a_movie_and_fetch_it(self):
+        self.assertIn('Movie', self.browser.title)
+
+
+if __name__ == 'main':
+    unittest.main()
